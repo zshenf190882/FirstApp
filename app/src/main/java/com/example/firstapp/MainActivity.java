cb01e3270762927aebf92e61818f35095172cf83
zshenf190882 <1908827735@qq.com>
2020-03-20 17:24:35 +0800

    事件绑定

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -8,18 +8,49 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView out;
+    EditText inp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        out = (TextView) findViewById(R.id.showText);
+
+       inp = (EditText) findViewById(R.id.inpText);
+        String str = inp.getText().toString();
+
+        Button btn= (Button)findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("email","onClick called...");
+                String str = inp.getText().toString();
+                out.setText("Hello "+str);
+
+            }
+        });
+
 
     }
 
+    @Override
+    public void onClick(View v) {
+        Log.i("clink","onClink......");
+
+        String str = inp.getText().toString();
+        out.setText("Hello "+str);
     }
+    public void btnClick(View btn){
+        Log.i("click","btnClick called....");
+    }
+}
 
