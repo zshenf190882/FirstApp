a908c91e0a07ee826ccef3b1ea1c543bac8954dd
zshenf190882 <1908827735@qq.com>
2020-04-16 16:05:05 +0800

    Activity生命周期及数据保存

--- app/src/main/java/com/example/firstapp/Main3Activity.java
+++ app/src/main/java/com/example/firstapp/Main3Activity.java
@@ -1,22 +1,46 @@
 package com.example.firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Main3Activity extends AppCompatActivity {
  TextView score;
     TextView score2;
+    private final String TAG="second";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
        score=(TextView) findViewById(R.id.score);
         score2=(TextView) findViewById(R.id.scoreb);
+        Log.i(TAG,"onCreat: ");
     }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String scorea=((TextView) findViewById(R.id.score)).getText().toString();
+        String scoreb=((TextView) findViewById(R.id.scoreb)).getText().toString();;
+        Log.i(TAG,"onSaveInstanceState: ");
+        outState.putString("teama_score",scorea);
+        outState.putString("teamb_score",scoreb);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String scorea=savedInstanceState.getString("teama_score");
+        String scoreb=savedInstanceState.getString("teamb_score");
+        Log.i(TAG,"onRestoreInstanceState: ");
+        ((TextView) findViewById(R.id.score)).setText(scorea);
+        ((TextView) findViewById(R.id.scoreb)).setText(scoreb);
+    }
+
     public void btnAdd1(View btn){
         if(btn.getId()==R.id.btn_1){
         showScore(1);}
