7049cebeb63b8bb92da7ee4e9c059d951b6ac14a
zshenf190882 <1908827735@qq.com>
2020-06-08 22:06:36 +0800

    用数据库保存数据，，

--- app/src/main/java/com/example/firstapp/RateListActivity.java
+++ app/src/main/java/com/example/firstapp/RateListActivity.java
@@ -8,9 +8,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListAdapter;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -25,112 +23,103 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class RateListActivity extends ListActivity implements Runnable {
+public class RateListActivity extends ListActivity implements Runnable{
 
-    EditText key;
-    TextView show2;
+    String data[] = {"one","two","three"};
     Handler handler;
+    private String TAG = "ListActivity";
     private String logDate = "";
-    private final String DATE_SP_KEY = "lastRateDateStr";
-
-    private final String TAG = "Rate";
+    private final  String DATE_SP_KEY = "lastRateDateStr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_rate_list); //因为父类已经包含一个布局，不需要再一个布局
+
         SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
-        logDate = sp.getString(DATE_SP_KEY, "");
-        Log.i("List", "lastRateDateStr=" + logDate);
+        logDate = sp.getString(DATE_SP_KEY,"");
+        Log.i("List","lastRateDateSt"+logDate);
 
-        List<String> list1 = new ArrayList<>();
-        for (int i = 1; i < 100; i++) {
-            list1.add("item" + i);
+
+        List<String> list1 = new ArrayList<String>();
+        for(int i=1;i<100;i++){
+            list1.add("item"+i);
         }
-
-        //setContentView(R.layout.activity_rate_list);
-        String data[] = {"wait..."};
-        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list1);
         setListAdapter(adapter);
+
         Thread t = new Thread(this);
         t.start();
-//
-        handler = new Handler() {
+
+        handler = new Handler(){
             @Override
             public void handleMessage(@NonNull Message msg) {
-                if (msg.what == 7) {
+                if(msg.what == 7){
                     List<String> list2 = (List<String>) msg.obj;
-                    ListAdapter adapter = new ArrayAdapter<String>(RateListActivity.this, android.R.layout.simple_list_item_1, list2);
+                    ListAdapter adapter = new ArrayAdapter<String>(RateListActivity.this,android.R.layout.simple_list_item_1,list2);
                     setListAdapter(adapter);
                 }
-
                 super.handleMessage(msg);
+
             }
         };
-
     }
 
     @Override
     public void run() {
-//获取网络数据，放入list带回到到主线程中
-        Log.i("List", "run..");
+        //获取网络数据，放入list带回主线程中
         List<String> retList = new ArrayList<String>();
         String curDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
-        Log.i("run", "curDateStr:" + curDateStr + " logDate:" + logDate);
-        if (curDateStr.equals(logDate)) {
-            //如果相等，则不从网络中获取数据
-            Log.i("run", "日期相等，从数据库中获取数据");
+        Log.i("run","curDateStr"+curDateStr+"logDate"+logDate);
+
+        if(curDateStr.equals(logDate)){
+            Log.i("run","日期相等，从数据库中获取数据");
             RateManager manager = new RateManager(this);
-                for(RateItem rateItem : manager.listAll()){
-                        retList.add(rateItem.getCurName() + "-->" + rateItem. getCurRate());
-                        retList.add("hh");
-
-                Log.i(TAG, "onOptionsItemSelected:取出数据" + rateItem.getCurRate());
+            for(RateItem item:manager.listAll()){
+                retList.add(item.getCurName()+"-->"+item.getCurRate());
             }
-            retList.add("sdsf");
-
-
         }else {
-            Log.i("run", "日期不相等，从网络中获取在线数据");
+            Log.i("run","日期不相等，从网络中获取数据");
             Document doc = null;
-
             try {
-                Thread.sleep(1000);
-                doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj/").get();
-                Log.i(TAG, "run:" + doc.title());
+                doc = Jsoup.connect("https://www.usd-cny.com/bankofchina.htm").get();
+                Log.i(TAG,"run:"+doc.title());
                 Elements tables = doc.getElementsByTag("table");
-                Element table2 = tables.get(1);
-                //获取TD中的元素
-                Elements tds = table2.getElementsByTag("td");
+                Element table1 = tables.get(0);
+                Elements tds = table1.getElementsByTag("td");
+
                 List<RateItem> rateList = new ArrayList<RateItem>();
-                for (int i = 0; i < tds.size(); i += 8) {
+                for (int i =0;i<tds.size();i+=6){
                     Element td1 = tds.get(i);
-                    Element td2 = tds.get(i + 5);
+                    Element td2 = tds.get(i+5);
+                    Log.i(TAG,"run:"+td1.text()+"==>"+td2.text());
                     String str1 = td1.text();
                     String val = td2.text();
-                    Log.i(TAG, "run: text=" + td1.text());
-                    retList.add(str1 + "==>" + val);
+                    retList.add(str1+"==>"+val);
+                    rateList.add(new RateItem(str1,val));
+                }
 
-
-                    rateList.add(new RateItem(str1, val));
-//
-                } //把数据写入数据库中
+                //把数据写入到数据库中
                 RateManager manager = new RateManager(this);
                 manager.deleteAll();
                 manager.addAll(rateList);
+                Log.i("run","数据已写入数据库中："+curDateStr);
 
-                //记录更新日期
-                SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
+                //更新记录日期
+                SharedPreferences sp = getSharedPreferences("myrate",Context.MODE_PRIVATE);
                 SharedPreferences.Editor edit = sp.edit();
-                edit.putString(DATE_SP_KEY, curDateStr);
+                edit.putString(DATE_SP_KEY,curDateStr);
                 edit.commit();
-                Log.i("run", "更新日期結束: " + curDateStr);
-            } catch (IOException | InterruptedException e) {
+                Log.i("run","更新日期结束："+curDateStr);
+
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            Message msg = handler.obtainMessage(7);
-            msg.obj = retList;
-            handler.sendMessage(msg);
         }
-    }
 
+        Message msg  = handler.obtainMessage(7);
+        msg.obj = retList;
+        handler.sendMessage(msg);
+    }
 }
+
